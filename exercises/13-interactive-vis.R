c096b206e1f4a549bc7613b086134b87cfe46b52
robbfitzsimmons <robb@linnaean.co>
2017-10-08 15:32:47 -0400

    Completed interactive visualization exercise.

--- exercises/13-interactive-vis.R
+++ exercises/13-interactive-vis.R
@@ -1,1 +1,22 @@
+library(shiny)
+library(ggplot2)
+diamonds2 <- diamonds[sample(1:nrow(diamonds), 5000), ]
 
+ui <- fluidPage(
+  plotOutput("plot1", click = "click"),
+  verbatimTextOutput("location")
+  )
+
+server <- function(input, output) {
+  output$plot1 <- renderPlot({
+    ggplot(diamonds2, aes(x = cut, y = color)) +
+      geom_count() +
+      theme_bw()
+  })
+  output$location <- renderPrint({
+    cat("x=",input$click$x, "\ny=", input$click$y)
+  })
+  
+}
+
+shinyApp(ui, server)
