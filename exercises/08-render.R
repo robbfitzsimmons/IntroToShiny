0a05aaf7b7612b9f2b06874435f7eb2269687b6d
robbfitzsimmons <robb@linnaean.co>
2017-10-07 23:49:21 -0400

    Rendered summary statistics; added reactivity to sync stats with histogram random number generation.

--- exercises/08-render.R
+++ exercises/08-render.R
@@ -1,1 +1,18 @@
+library(shiny)
 
+ui <- fluidPage(
+  sliderInput("num", "Choose a number:", 1, 100, 50),
+  plotOutput("hist"),
+  verbatimTextOutput("sum")
+)
+
+server <- function(input, output) {
+  output$hist <- renderPlot({
+    hist(rnorm(input$num))
+  })
+  output$sum <- renderPrint({
+    (summary(rnorm(input$num)))
+  })
+}
+
+shinyApp(ui = ui, server = server)
--- exercises/09-reactive.R
+++ exercises/09-reactive.R
@@ -1,1 +1,21 @@
+library(shiny)
 
+ui <- fluidPage(
+  sliderInput("num", "Choose a number:", 1, 100, 50),
+  plotOutput("hist"),
+  verbatimTextOutput("sum")
+)
+
+server <- function(input, output) {
+  data <- reactive({
+    rnorm(input$num)
+  })
+  output$hist <- renderPlot({
+    hist(data())
+  })
+  output$sum <- renderPrint({
+    summary(data())
+  })
+}
+
+shinyApp(ui = ui, server = server)
