66f5a282e825963ce64040bc81c1c61a5ac1ac40
robbfitzsimmons <robb@linnaean.co>
2017-10-07 23:59:26 -0400

    Add an action-event button.

---
+++ exercises/10-eventReactive.R
@@ -0,0 +1,22 @@
+library(shiny)
+
+ui <- fluidPage(
+  sliderInput("num", "Choose a number:", 1, 100, 50),
+  actionButton("go", "Update"),
+  plotOutput("hist"),
+  verbatimTextOutput("sum")
+)
+
+server <- function(input, output) {
+  data <- eventReactive(input$go,{
+    rnorm(input$num)
+  })
+  output$hist <- renderPlot({
+    hist(data())
+  })
+  output$sum <- renderPrint({
+    summary(data())
+  })
+}
+
+shinyApp(ui = ui, server = server)
