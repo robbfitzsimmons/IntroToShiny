8438a7f8bea950e07b4b7dcf60bcce644eb68a6f
robbfitzsimmons <robb@linnaean.co>
2017-10-08 13:34:40 -0400

    Completed invalidateLater exercise.

--- exercises/11-invalidateLater.R
+++ exercises/11-invalidateLater.R
@@ -1,1 +1,15 @@
 
+library(shiny)
+
+ui <- fluidPage(
+  verbatimTextOutput("time")
+)
+
+server <- function(input, output){
+  output$time <- renderPrint({
+    invalidateLater(1000)
+    Sys.time()
+  })
+}
+
+shinyApp(ui, server)
