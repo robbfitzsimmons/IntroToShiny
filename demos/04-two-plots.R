195b80931a3330c88ba090d7234da30e33c9af5f
robbfitzsimmons <robb@linnaean.co>
2017-10-09 00:17:55 -0400

    Completed brush-synced plots exercise.

--- demos/04-two-plots.R
+++ demos/04-two-plots.R
@@ -3,7 +3,7 @@ library(ggplot2)
 diamonds2 <- diamonds[sample(1:nrow(diamonds), 5000), ]
 
 ui <- fluidPage(
-  plotOutput("plot1"),
+  plotOutput("plot1", brush = "brush"),
   plotOutput("plot2")
 )
 
@@ -11,12 +11,17 @@ server <- function(input, output) {
   output$plot1 <- renderPlot({
     ggplot(diamonds2, aes(x = cut, y = color)) +
       geom_count() +
-      theme_bw()
-  })
+      theme_bw() +
+      geom_count(data = brushedPoints(diamonds, input$brush), color = "green")
+})
   output$plot2 <- renderPlot({
     ggplot(diamonds2, aes(carat, price)) +
       geom_point() +
-      theme_bw()
+      theme_bw() +
+      geom_count(data = brushedPoints(diamonds, input$brush), color = "green")
+  })
+  output$location <- renderPrint({
+    brushedPoints(diamonds, input$brush)
   })
 }
 
