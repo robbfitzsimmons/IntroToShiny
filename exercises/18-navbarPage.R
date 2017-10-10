ab5aadd0858a1780cadbc76234b624de9360112d
robbfitzsimmons <robb@linnaean.co>
2017-10-10 13:45:02 -0700

    Panels and navbarPage exercises.

--- exercises/18-navbarPage.R
+++ exercises/18-navbarPage.R
@@ -1,1 +1,24 @@
+library("shiny")
 
+ui <- navbarPage("My App",
+  tabPanel("Slider",
+    sliderInput(inputId = "num", 
+                label = "Choose a number", 
+                val = 25, min = 1, max = 100)
+  ),
+    tabPanel("Plot", plotOutput("hist")),
+    tabPanel("Summary", verbatimTextOutput("sum"))
+)
+
+
+server <- function(input, output) {
+  data <- reactive({rnorm(input$num)})
+  output$hist <- renderPlot({
+    hist(data())   
+  })
+  output$sum <- renderPrint({
+    summary(data())
+  })
+}
+
+shinyApp(ui = ui, server = server)
