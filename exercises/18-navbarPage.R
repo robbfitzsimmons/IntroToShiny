b809653795e59c673b9cead212537e50453f1d2f
robbfitzsimmons <robb@linnaean.co>
2017-10-10 13:53:58 -0700

    Corrected navbarPage exercise.

--- exercises/18-navbarPage.R
+++ exercises/18-navbarPage.R
@@ -1,24 +1,32 @@
-library("shiny")
+library(shiny)
 
-ui <- navbarPage("My App",
-  tabPanel("Slider",
-    sliderInput(inputId = "num", 
-                label = "Choose a number", 
-                val = 25, min = 1, max = 100)
-  ),
-    tabPanel("Plot", plotOutput("hist")),
-    tabPanel("Summary", verbatimTextOutput("sum"))
+ui <- navbarPage(title = "Random generator",
+                  tabPanel("Normal data",
+                  column(width = 2,
+                         actionButton("renorm", "Sample")),
+                  column(width = 10,
+                         plotOutput("norm"))),
+                  tabPanel("Uniform data",
+                  column(width = 2,
+                         actionButton("reunif", "Sample")),
+                  column(width = 10,
+                         plotOutput("unif"))),
+                  tabPanel("Chi Squared data",
+                  column(width = 2,
+                         actionButton("rechisq", "Sample")),
+                  column(width = 10,
+                         plotOutput("chisq")))
 )
 
-
 server <- function(input, output) {
-  data <- reactive({rnorm(input$num)})
-  output$hist <- renderPlot({
-    hist(data())   
-  })
-  output$sum <- renderPrint({
-    summary(data())
-  })
+  
+  normdata <- eventReactive(input$renorm, {rnorm(100)})
+  unifdata <- eventReactive(input$reunif, {runif(100)})
+  chisqdata <- eventReactive(input$rechisq, {rchisq(100, df = 2)})
+  
+  output$norm <- renderPlot({hist(normdata(), breaks = 30, col = "grey", bor = "white")})
+  output$unif <- renderPlot({hist(unifdata(), breaks = 30, col = "grey", bor = "white")})
+  output$chisq <- renderPlot({hist(chisqdata(), breaks = 30, col = "grey", bor = "white")})
 }
 
-shinyApp(ui = ui, server = server)
+shinyApp(server = server, ui = ui)
